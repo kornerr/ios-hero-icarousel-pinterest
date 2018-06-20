438f38fcd0a17773b135a00a56c92653a0f499ab
Michael Kapelko <kornerr@gmail.com>
2018-06-20 17:58:51 +0300

    Calculate layout in prepare()

--- App/Collection/PinterestLayout.swift
+++ App/Collection/PinterestLayout.swift
@@ -1,6 +1,11 @@
 
 import UIKit
 
+private func PINTEREST_LAYOUT_LOG(_ message: String)
+{
+    NSLog("PinterestLayout \(message)")
+}
+
 class PinterestLayout: UICollectionViewFlowLayout
 {
 
@@ -8,6 +13,7 @@ class PinterestLayout: UICollectionViewFlowLayout
 
     override func prepare()
     {
+        PINTEREST_LAYOUT_LOG("prepare")
         self.setupItemSizes()
     }
 
@@ -28,6 +34,7 @@ class PinterestLayout: UICollectionViewFlowLayout
 
     // MARK: - ITEM SIZES
 
+    // NOTE Make sure to provide item sizes before first rendering.
     var itemSizes = [CGSize]()
 
     private var columnsCount = 2
@@ -36,6 +43,54 @@ class PinterestLayout: UICollectionViewFlowLayout
 
     private func setupItemSizes()
     {
+        guard
+            self.cache.isEmpty == true,
+            self.itemSizes.isEmpty == false,
+            let collectionView = self.collectionView
+        else
+        {
+            return
+        }
+        let columnWidth = self.contentWidth / CGFloat(self.columnsCount)
+        var xOffsets = [CGFloat]()
+        for column in 0..<self.columnsCount
+        {
+            xOffsets.append(CGFloat(column) * columnWidth)
+        }
+        var yOffsets = [CGFloat](repeating: 0, count: self.columnsCount)
+
+        var column = 0
+        for id in 0..<collectionView.numberOfItems(inSection: 0)
+        {
+            let size = self.itemSizes[id]
+            // Calculate factor to resize item to:
+            // * make item's width match columnWidth
+            // * keep item's aspect ratio
+            let factor = size.width / columnWidth
+            // Calculate new height keeping original aspect ratio.
+            let height = size.height * factor + self.cellPadding * 2
+            let frame =
+                CGRect(
+                    x: xOffsets[column],
+                    y: yOffsets[column],
+                    width: columnWidth,
+                    height: height
+                )
+            let insetFrame =
+                frame.insetBy(dx: self.cellPadding, dy: self.cellPadding)
+
+            let indexPath = IndexPath(item: id, section: 0)
+            let attributes =
+                UICollectionViewLayoutAttributes(forCellWith: indexPath)
+            attributes.frame = insetFrame
+            self.cache.append(attributes)
+
+            self.contentHeight = max(self.contentHeight, frame.maxY)
+            yOffsets[column] = yOffsets[column] + height
+
+            // Toggle column between 1 and 0.
+            column = column < (self.columnsCount - 1) ? (column + 1) : 0
+        }
     }
 
 
